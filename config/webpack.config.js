afe96e1d3149b112c19ffd9d80c91bd3b1114c88
kisstar <dwh.chn@foxmail.com>
2020-03-16 21:26:14 +0800

    build(lint): useing stylelint in webpack

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -24,6 +24,7 @@ const getClientEnvironment = require('./env');
 const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 const postcssNormalize = require('postcss-normalize');
 
@@ -526,6 +527,14 @@ module.exports = function(webpackEnv) {
             : undefined,
         ),
       ),
+      // lint your styles
+      isEnvDevelopment &&
+        new StylelintPlugin({
+          context: 'src',
+          files: '**/*.less',
+          failOnError: false,
+          syntax: 'less',
+        }),
       // Inlines the webpack runtime script. This script is too small to warrant
       // a network request.
       // https://github.com/facebook/create-react-app/issues/5358
